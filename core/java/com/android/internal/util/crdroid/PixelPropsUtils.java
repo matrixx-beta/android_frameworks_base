905971c6fd9dc0d496e4846ee195d046c486a45d
Kshitij Koyande <koyande72@gmail.com>
2025-04-09 15:21:05 +0530

    Revert "PixelPropsUtils: Move certified props to vendor"
    This reverts commit 5359d1d9e726e27671c3659e9e8d08e977e441e8.

--- core/java/com/android/internal/util/crdroid/PixelPropsUtils.java
+++ core/java/com/android/internal/util/crdroid/PixelPropsUtils.java
@@ -24,8 +24,6 @@ import android.os.Build;
 import android.os.SystemProperties;
 import android.util.Log;
 
-import com.android.internal.R;
-
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -162,12 +160,7 @@ public final class PixelPropsUtils {
             "com.vng.pubgmobile"
     };
 
-<<<<<<< HEAD
-    private static volatile List<String> sCertifiedProps = new ArrayList<>();
-=======
     private static volatile boolean sIsFinsky = false;
-    private static volatile String[] sCertifiedProps;
->>>>>>> 4a9e6ba92ff7 (Revert "PixelPropsUtils: Dynamically spoof props for GMS")
 
     static {
         propsToChangeGeneric = new HashMap<>();
@@ -266,7 +259,7 @@ public final class PixelPropsUtils {
             } else if (packageName.equals("com.google.android.gms")) {
                 final String processName = Application.getProcessName().toLowerCase();
                 if (processName.contains("unstable")) {
-                    spoofBuildGms(context);
+                    spoofBuildGms();
                     return;
                 }
                 return;
@@ -404,20 +397,23 @@ public final class PixelPropsUtils {
         }
     }
 
-    private static void spoofBuildGms(Context context) {
+    private static void spoofBuildGms() {
         if (!SystemProperties.getBoolean(SPOOF_PIXEL_PI, true))
             return;
         // Alter build parameters to avoid hardware attestation enforcement
-        sCertifiedProps = context.getResources().getStringArray(R.array.config_certifiedBuildProperties);
-        for (String entry : sCertifiedProps) {
-            // Each entry must be of the format FIELD:value
-            final String[] fieldAndProp = entry.split(":", 2);
-            if (fieldAndProp.length != 2) {
-                Log.e(TAG, "Invalid entry in certified props: " + entry);
-                continue;
-            }
-            setPropValue(fieldAndProp[0], fieldAndProp[1]);
-        }
+        setPropValue("MANUFACTURER", "Google");
+        setPropValue("MODEL", "Pixel 9 Pro XL");
+        setPropValue("FINGERPRINT", "google/komodo_beta/komodo:15/AP41.240925.009/12534705:user/release-keys");
+        setPropValue("BRAND", "google");
+        setPropValue("PRODUCT", "komodo_beta");
+        setPropValue("DEVICE", "komodo");
+        setPropValue("VERSION.RELEASE", "15");
+        setPropValue("ID", "AP41.240925.009");
+        setPropValue("VERSION.INCREMENTAL", "12534705");
+        setPropValue("TYPE", "user");
+        setPropValue("TAGS", "release-keys");
+        setPropValue("VERSION.SECURITY_PATCH", "2024-10-05");
+        setPropValue("VERSION.DEVICE_INITIAL_SDK_INT", "32");
     }
 
     private static boolean isCallerSafetyNet() {
